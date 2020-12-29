5055bbb11ea410cff6cf92799f5c3a292b956a22
ier <r05k@mail.ru>
2020-12-30 01:15:24 +0300

    05

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import Header from "./comp/Header/Header";
 import Home from "./comp/Pages/Home";
 import Cart from "./comp/Pages/Cart";
 import {Route} from "react-router-dom";
+import axios from "axios";
+
 
 function App() {
+    const [pizzzas, serPizzzas] = React.useState([]);
+    /*    React.useEffect(() => {
+            fetch('http://localhost:3000/db.json')
+                .then((resp) => resp.json())
+                .then(json => {
+                    serPizzzas(json.pizzas);
+                });
+        }, []); */
+    React.useEffect(() => {
+        axios.get('http://localhost:3000/db.json').then(({data}) => {
+            serPizzzas(data.pizzas);
+        });
+    }, []);
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
-                <Route path='/' component={Home} exact/>
+                <Route path='/' render={() => <Home items={pizzzas}/>} exact/>
                 <Route path='/cart' component={Cart} exact/>
             </div>
         </div>
